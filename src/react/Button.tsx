90a7416d4a31bddc78e673bd704f2576de7a255d
Kevin04c <kevincespedes2004@gmail.com>
2023-01-14 17:10:55 -0500

    feat: add props target

--- src/react/Button.tsx
+++ src/react/Button.tsx
@@ -1,16 +1,16 @@
 interface Props {
   href: string;
   text: string;
-  type: 'primary' | 'secondary',
+  type: 'primary' | 'secondary';
+  target?: '_blank' | '_self';
 }
-
-export const Button = ({ href, text, type }: Props) => {
+export const Button = ({ href, text, type, target = '_self' }: Props) => {
   const buttonClass =
     type === 'primary'
-      ? "bg-primary-900 text-white shadow-lg shadow-primary-900/30 hover:bg-primary-800 hover:opacity-90 transition-all"
-      : "bg-transparent border border-primary-900 text-primary-900 hover:shadow-md hover:shadow-primary-900/30 transition-all  duration-100";
+      ? 'bg-primary-900 text-white shadow-lg shadow-primary-900/30 hover:bg-primary-800 hover:opacity-90 transition-all'
+      : 'bg-transparent border border-primary-900 text-primary-900 hover:shadow-md hover:shadow-primary-900/30 transition-all  duration-100';
   return (
-    <a href={href} className={`px-7 py-1 rounded-md ${buttonClass}`} target="_blank">
+    <a href={href} className={`px-7 py-1 rounded-md ${buttonClass}`} target={target}>
       {text}
     </a>
   );
