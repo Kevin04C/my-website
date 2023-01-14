605008a3936b2de08acd47a84cbdd097c8fc8092
Kevin04c <kevincespedes2004@gmail.com>
2023-01-14 16:20:17 -0500

    feat: add remark-reading-time.msj to the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,29 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx({
-    syntaxHighlight: 'shiki'
-  })],
+  integrations: [
+    tailwind(),
+    react(),
+    mdx({
+      syntaxHighlight: 'shiki',
+    }),
+  ],
   markdown: {
+    remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       // theme: 'material-theme-darker',
-      theme: "one-dark-pro"
-    }
-  }
+      theme: 'one-dark-pro',
+    },
+  },
 });
