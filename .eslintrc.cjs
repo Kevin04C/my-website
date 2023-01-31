1f5ac5922d25e15176f38d09533041dbf9a9787d
Kevin04C <kevincespedes2004@gmail.com>
2023-01-30 21:52:23 -0500

    feat: add support typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,25 @@
 module.exports = {
-  extends: ["plugin:astro/recommended"],
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ['*.astro'],
       // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
+      parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {
-        "astro/semi": [
-          "error",
-          "always",
-        ],
-        "quotes": [
-          "error",
-          "single",
-        ],
+        'astro/semi': ['error', 'always'],
+        quotes: ['warn', 'single'],
       },
     },
   ],
-}
+};
