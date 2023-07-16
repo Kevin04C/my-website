7fef4c50f0e16e8f49fffe5ce230903e6030b311
Kevin04c <kevincespedes2004@gmail.com>
2023-07-16 18:32:26 -0500

    Feat: Add skills

---
+++ src/constans/SkillsIcons.ts
@@ -0,0 +1,68 @@
+import JavascriptIcon from '../components/icons/JavascriptIcon.astro';
+import TypescriptIcon from '../components/icons/TypescriptIcon.astro';
+import PrismaIcon from '../components/icons/PrismaIcon.astro';
+import ReactIcon from '../components/icons/ReactIcon.astro';
+import NextjsIcon from '../components/icons/NextjsIcon.astro';
+import MySQLIcon from '../components/icons/MySQLIcon.astro';
+import TailwindCSSIcon from '../components/icons/TailwindCSSIcon.astro';
+import NodeJSIcon from '../components/icons/NodeJSIcon.astro';
+import GitIcon from '../components/icons/GitIcon.astro';
+import SassIcon from '../components/icons/SassIcon.astro';
+import FirebaseIcon from '../components/icons/FirebaseIcon.astro';
+import MongoDBIcon from '../components/icons/MongoDBIcon.astro';
+import SQLServerIcon from '../components/icons/SQLServerIcon.astro';
+
+export const SkillsIcons = [
+  {
+    name: 'React',
+    Icon: ReactIcon,
+  },
+  {
+    name: 'Next.js',
+    Icon: NextjsIcon,
+  },
+  {
+    name: 'JavaScript',
+    Icon: JavascriptIcon,
+  },
+  {
+    name: 'TypeScript',
+    Icon: TypescriptIcon,
+  },
+  {
+    name: 'Prisma',
+    Icon: PrismaIcon,
+  },
+  {
+    name: 'MySQL',
+    Icon: MySQLIcon,
+  },
+  {
+    name: 'SQL Server',
+    Icon: SQLServerIcon,
+  },
+  {
+    name: 'MongoDB',
+    Icon: MongoDBIcon,
+  },
+  {
+    name: 'Firebase',
+    Icon: FirebaseIcon,
+  },
+  {
+    name: 'Node.js  ',
+    Icon: NodeJSIcon,
+  },
+  {
+    name: 'Git',
+    Icon: GitIcon,
+  },
+  {
+    name: 'Sass',
+    Icon: SassIcon,
+  },
+  {
+    name: 'Tailwind CSS',
+    Icon: TailwindCSSIcon,
+  },
+];
