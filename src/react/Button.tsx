2def17ce8eb78431fa0af8357b79f3b42165e4ff
Kevin04c <kevincespedes2004@gmail.com>
2023-07-16 18:51:06 -0500

    Feat: Update Button

--- src/react/Button.tsx
+++ src/react/Button.tsx
@@ -1,16 +1,21 @@
-interface Props {
+import { AnchorHTMLAttributes, DetailedHTMLProps, FC } from 'react';
+
+type Props = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
   href: string;
   text: string;
   type: 'primary' | 'secondary';
-  target?: '_blank' | '_self';
-}
-export const Button = ({ href, text, type, target = '_self' }: Props) => {
-  const buttonClass =
-    type === 'primary'
-      ? 'bg-primary-900 text-white shadow-lg shadow-primary-900/30 hover:bg-primary-800 hover:opacity-90 transition-all'
-      : 'bg-transparent border border-primary-900 text-primary-900 hover:shadow-md hover:shadow-primary-900/30 transition-all  duration-100';
+};
+
+export const Button: FC<Props> = ({ href, text, type, target = '_self', ...rest }) => {
+  const variants = {
+    primary:
+      'bg-primary-900 text-white shadow-lg shadow-primary-900/30 hover:bg-primary-800 hover:opacity-90 transition-all',
+    secondary:
+      'bg-transparent border border-primary-900 text-primary-900 hover:shadow-md hover:shadow-primary-900/30 transition-all  duration-100',
+  };
+
   return (
-    <a href={href} className={`px-7 py-1 rounded-md ${buttonClass}`} target={target}>
+    <a href={href} className={`px-7 py-1 rounded-md ${variants[type]}`} target={target} {...rest}>
       {text}
     </a>
   );
